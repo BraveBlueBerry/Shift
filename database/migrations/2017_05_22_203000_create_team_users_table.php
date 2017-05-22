b709179e25dbba4da94b1daa95aee673ef9e6826
Wouter <wouterhoutsma@gmail.com>
2017-05-22 23:47:41 +0200

    Added basic models for the App database. Added migrations.

---
+++ database/migrations/2017_05_22_203000_create_team_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->integer('team')->unsigned();
+            $table->integer('user')->unsigned();
+            $table->foreign('user')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('team')
+                  ->references('id')
+                  ->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_users');
+    }
+}
