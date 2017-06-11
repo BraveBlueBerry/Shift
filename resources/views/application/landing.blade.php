251bcd63665c9c8559a5e9947fc3f3f4dafe11eb
Jesse <jesse.tijsma@gmail.com>
2017-06-11 20:47:51 +0200

    Jquery is nu wat netter
    En overzichtelijker. Raadpleeg whapp voor uitleg. Niet heel moeilijk ofzo :p

--- resources/views/application/landing.blade.php
+++ resources/views/application/landing.blade.php
@@ -7,50 +7,40 @@
         @include('application.parts.navigationText')
       </div>
       <div id="wrapperbb">
-        <div id="contentbb">
-          <div class="uk-text-lead headText app-headerbb">Welkom gebruiker</div>
-          <div class="page-panelbb">
-            <div class="page-panel-innerbb">
-              <div class="page-panel-contentbb maskablebb" id="dashboard-containerbb">
-                <div id="dashboard-overviewbb" class="dashboard-sectionbb">
-                  <div class="dashboard-overviewbb">
-                    <section class="dashboard-left-contentbb">
-                      <i class="fa fa-check-square-o fa-5x" id="check" aria-hidden="true"></i> <br />
-                      Op dit moment heb je geen uren die je nog moet opsturen.
-                    </section>
-                    <section class="dashboard-right-contentbb">
-                      <img class="uk-align-center" width="180" height="165" src="{{url('storage/logozwartwit.png')}}" alt="">
-                      Gelijk een registratie toevoegen?
-                      <a href="#">Dat kan hier</a>
-                    </section>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          @include('application.actualLanding')
           @include('application.overzicht')
+          @include('application.teams')
       </div>
     </div>
   </body>
   <script>
       $(function() {
-          $("#contentbb").hide();
-          $("#contentbb2").hide();
+          $("#contentbb_overzicht").hide();
+          $("#contentbb_teams").hide();
       });
 
       $(function(){
           $('.knop1').click(function() {
-              $("#contentbb").hide();
-              $("#contentbb2").show();
+              $("#contentbb_landing").hide();
+              $("#contentbb_teams").hide();
+              $("#contentbb_overzicht").show();
           });
       });
 
     $(function(){
         $('.knop2').click(function() {
-            $("#contentbb2").hide();
-            $("#contentbb").show();
+            $("#contentbb_overzicht").hide();
+            $("#contentbb_landing").hide();
+            $("#contentbb_teams").show();
         });
     });
+
+      $(function(){
+          $('.landingknop').click(function() {
+              $("#contentbb_overzicht").hide();
+              $("#contentbb_teams").hide();
+              $("#contentbb_landing").show();
+          });
+      });
   </script>
 </html>
--- resources/views/application/overzicht.blade.php
+++ resources/views/application/overzicht.blade.php
@@ -1,50 +1,49 @@
-
-        <div id="contentbb2">
-          <div class="uk-text-lead headText app-headerbb">Overzicht uren</div>
-          <div class="page-panelbb">
-            <div class="page-panel-innerbb">
-              <div class="page-panel-contentbb maskablebb" id="dashboard-containerbb">
+<div id="contentbb_overzicht">
+    <div class="uk-text-lead headText app-headerbb">Overzicht uren</div>
+    <div class="page-panelbb">
+        <div class="page-panel-innerbb">
+            <div class="page-panel-contentbb maskablebb" id="dashboard-containerbb">
                 <div id="dashboard-overviewbb" class="dashboard-sectionbb">
-                  <div class="dashboard-overviewbb">
-                    <form class="uk-form-horizontal uk-margin-large">
-                      <div class="uk-margin">
+                    <div class="dashboard-overviewbb">
+                        <form class="uk-form-horizontal uk-margin-large">
+                            <div class="uk-margin">
 
-                        <div class="left">
+                                <div class="left">
 
-                        <label class="uk-form-label" for="form-horizontal-select">Wanneer </label>
-                        <div class="uk-form-controls">
-                          <select class="uk-select" id="form-horizontal-select">
-                            <!-- Deze opties staan niet vast, misschien moeten we juist data kiezen die gebruikt zijn in de geregistreerde uren
-                                  Datepicker van maken met opties "van" en "tot" -->
-                            <option>Alles</option>
-                            <option>Vandaag</option>
-                            <option>Afgelopen week</option>
-                            <option>Afgelopen 2 weken</option>
-                            <option>Afgelopen maand</option>
-                          </select>
-                        </div>
+                                    <label class="uk-form-label" for="form-horizontal-select">Wanneer </label>
+                                    <div class="uk-form-controls">
+                                        <select class="uk-select" id="form-horizontal-select">
+                                            <!-- Deze opties staan niet vast, misschien moeten we juist data kiezen die gebruikt zijn in de geregistreerde uren
+                                                  Datepicker van maken met opties "van" en "tot" -->
+                                            <option>Alles</option>
+                                            <option>Vandaag</option>
+                                            <option>Afgelopen week</option>
+                                            <option>Afgelopen 2 weken</option>
+                                            <option>Afgelopen maand</option>
+                                        </select>
+                                    </div>
 
-                        </div>
-                        <div class="right" style="margin-left:20px;">
+                                </div>
+                                <div class="right" style="margin-left:20px;">
 
-                        <label class="uk-form-label" for="form-horizontal-select">Categorie</label>
-                        <div class="uk-form-controls">
-                          <select class="uk-select" id="form-horizontal-select">
-                            <!-- Deze opties staan niet vast, moet samengaan met de categoriën die de gebruiker aangemaakt heeft -->
-                            <option>Alles</option>
-                            <option>Blauw</option>
-                            <option>Groen</option>
-                            <option>Zwart</option>
-                          </select>
-                        </div>
+                                    <label class="uk-form-label" for="form-horizontal-select">Categorie</label>
+                                    <div class="uk-form-controls">
+                                        <select class="uk-select" id="form-horizontal-select">
+                                            <!-- Deze opties staan niet vast, moet samengaan met de categoriën die de gebruiker aangemaakt heeft -->
+                                            <option>Alles</option>
+                                            <option>Blauw</option>
+                                            <option>Groen</option>
+                                            <option>Zwart</option>
+                                        </select>
+                                    </div>
 
-                      </div>
+                                </div>
 
+                            </div>
+                        </form>
                     </div>
-                    </form>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
+    </div>
+</div>
